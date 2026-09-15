835b69670c7a3e46eb9993f4c5bfa94c1dbe5f6b
makkahlaundryservice <rupshajannatsumi@gmail.com>
2026-09-15 22:40:01 +0300

    Refactor service worker event listeners to use function syntax

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,95 @@
 const CACHE_NAME = "makkah-laundry-v3";
-const FILES_TO_CACHE = ["./","./index.html","./style.css","./script.js","./manifest.json"];
 
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
-  self.skipWaiting();
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json"
+];
+
+
+// Install
+self.addEventListener("install", function (event) {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+
+                return cache.addAll(FILES_TO_CACHE);
+
+            })
+
+    );
+
+    self.skipWaiting();
+
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
-  self.clients.claim();
+
+// Activate
+self.addEventListener("activate", function (event) {
+
+    event.waitUntil(
+
+        caches.keys().then(function (cacheNames) {
+
+            return Promise.all(
+
+                cacheNames.map(function (cacheName) {
+
+                    if (cacheName !== CACHE_NAME) {
+
+                        return caches.delete(cacheName);
+
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+
 });
 
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    fetch(event.request).then(response => {
-      if (response && response.ok) {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-      }
-      return response;
-    }).catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html")))
-  );
+
+// Fetch
+self.addEventListener("fetch", function (event) {
+
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(function (cachedResponse) {
+
+                if (cachedResponse) {
+
+                    return cachedResponse;
+
+                }
+
+                return fetch(event.request)
+                    .then(function (networkResponse) {
+
+                        return networkResponse;
+
+                    })
+                    .catch(function () {
+
+                        return caches.match("./index.html");
+
+                    });
+
+            })
+
+    );
+
 });
